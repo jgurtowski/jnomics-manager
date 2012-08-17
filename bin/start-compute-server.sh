53ee8600116eaf7442da0b47a0f68ff912dd03df
James Gurtowski <gurtowskij@james-basev14.novalocal>
2012-08-17 00:05:54 +0000

    fixed bugs in service

--- bin/start-compute-server.sh
+++ bin/start-compute-server.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_PATH=`dirname "$0"`
 
-nohup java -Djava.library.path=/home/james/sources/hadoop/lib/native/Linux-amd64-64 -cp ${SCRIPT_PATH}/../conf:${SCRIPT_PATH}/../lib/jnomics.jar edu.cshl.schatz.jnomics.manager.server.JnomicsComputeServer > /tmp/jnomics-compute-service.log
+nohup java -Djava.library.path=/home/james/sources/hadoop/lib/native/Linux-amd64-64 -cp ${SCRIPT_PATH}/../conf:${SCRIPT_PATH}/../lib/jnomics.jar edu.cshl.schatz.jnomics.manager.server.JnomicsComputeServer > /tmp/jnomics-compute-service.log &
--- bin/start-data-server.sh
+++ bin/start-data-server.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_PATH=`dirname "$0"`
 
-nohup java -cp ${SCRIPT_PATH}/../conf:${SCRIPT_PATH}/../lib/jnomics.jar edu.cshl.schatz.jnomics.manager.server.JnomicsDataServer > /tmp/jnomics-data-server.log
+nohup java -cp ${SCRIPT_PATH}/../conf:${SCRIPT_PATH}/../lib/jnomics.jar edu.cshl.schatz.jnomics.manager.server.JnomicsDataServer > /tmp/jnomics-data-server.log  &
