16b4e156ee33ec27ba295b1557e1916b808de3df
james <gurtowsk@cshl.edu>
2012-09-28 14:34:01 -0400

    added client ip logging

--- src/manager/edu/cshl/schatz/jnomics/manager/server/JnomicsComputeServer.java
+++ src/manager/edu/cshl/schatz/jnomics/manager/server/JnomicsComputeServer.java
@@ -3,6 +3,11 @@ package edu.cshl.schatz.jnomics.manager.server;
 
 import edu.cshl.schatz.jnomics.manager.api.JnomicsCompute;
 import edu.cshl.schatz.jnomics.manager.common.JnomicsApiConfig;
+import org.apache.thrift.ProcessFunction;
+import org.apache.thrift.TApplicationException;
+import org.apache.thrift.TException;
+import org.apache.thrift.TProcessor;
+import org.apache.thrift.protocol.*;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TThreadPoolServer;
 import org.apache.thrift.transport.*;
@@ -13,6 +18,20 @@ import java.util.Properties;
 
 public class JnomicsComputeServer {
 
+    public static class JnomicsComputeProcessorHax<I extends JnomicsCompute.Iface>
+            extends JnomicsCompute.Processor<I> implements TProcessor {
+
+        public JnomicsComputeProcessorHax(I iface) {
+            super(iface);
+        }
+
+        @Override
+        public boolean process(TProtocol in, TProtocol out) throws TException {
+            System.err.println("Processing from client: " + ((TSocket)in.getTransport()).getSocket().getInetAddress());
+            return super.process(in,out);
+        }
+    }
+
     public static void main(String []args) throws TTransportException, IOException {
 
         Properties prop = JnomicsApiConfig.getServerProperties();
@@ -20,9 +39,11 @@ public class JnomicsComputeServer {
         int port = Integer.parseInt(prop.getProperty("compute-server-port"));
 
         JnomicsComputeHandler handler = new JnomicsComputeHandler(prop);
-        JnomicsCompute.Processor processor = new JnomicsCompute.Processor(handler);
+        JnomicsCompute.Processor processor = new JnomicsComputeProcessorHax(handler);
+        //JnomicsCompute.Processor processor = new JnomicsCompute.Processor(handler);
 
         TServerTransport serverTransport = new TServerSocket(port);
+
         //TNonblockingServerTransport serverTransport = new TNonblockingServerSocket(port);
         //TServer server = new TNonblockingServer(new TNonblockingServer.Args(serverTransport).processor(processor));
         TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
--- src/manager/edu/cshl/schatz/jnomics/manager/server/JnomicsDataServer.java
+++ src/manager/edu/cshl/schatz/jnomics/manager/server/JnomicsDataServer.java
@@ -2,10 +2,14 @@ package edu.cshl.schatz.jnomics.manager.server;
 
 import edu.cshl.schatz.jnomics.manager.api.JnomicsData;
 import edu.cshl.schatz.jnomics.manager.common.JnomicsApiConfig;
+import org.apache.thrift.TException;
+import org.apache.thrift.TProcessor;
+import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TThreadPoolServer;
 import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TServerTransport;
+import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransportException;
 
 import java.io.IOException;
@@ -19,6 +23,22 @@ public class JnomicsDataServer {
 
     private static int DEFAULTPORT = 43345;
 
+
+
+    public static class JnomicsDataProcessorHax<I extends JnomicsData.Iface>
+            extends JnomicsData.Processor<I> implements TProcessor {
+
+        public JnomicsDataProcessorHax(I iface) {
+            super(iface);
+        }
+
+        @Override
+        public boolean process(TProtocol in, TProtocol out) throws TException {
+            System.err.println("Processing from client: " + ((TSocket)in.getTransport()).getSocket().getInetAddress());
+            return super.process(in,out);
+        }
+    }
+
     public static void main(String []args) throws TTransportException, IOException, InterruptedException {
 
         Properties prop = JnomicsApiConfig.getServerProperties();
@@ -27,7 +47,8 @@ public class JnomicsDataServer {
 
         JnomicsDataHandler handler = new JnomicsDataHandler(prop);
         Thread garbageCollectorThread = new Thread(new JnomicsHandleGarbageCollector(handler));
-        JnomicsData.Processor processor = new JnomicsData.Processor(handler);
+        JnomicsData.Processor processor = new JnomicsDataProcessorHax(handler);
+        //JnomicsData.Processor processor = new JnomicsData.Processor(handler);
 
         TServerTransport serverTransport = new TServerSocket(port);
         TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
