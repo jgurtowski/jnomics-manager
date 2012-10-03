fae518abb10acccdf275dcb42f390ac95710d074
james <gurtowsk@cshl.edu>
2012-10-03 12:46:33 -0400

    added environment paths for certs

--- bin/start-data-server.sh
+++ bin/start-data-server.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_PATH=`dirname "$0"`
 
-nohup java -cp ${SCRIPT_PATH}/../conf:${SCRIPT_PATH}/../lib/jnomics.jar edu.cshl.schatz.jnomics.manager.server.JnomicsDataServer > /tmp/jnomics-data-server.log  &
+nohup java -Djkserver_keystore=${SCRIPT_PATH}/../cert/keystore.jks -cp ${SCRIPT_PATH}/../conf:${SCRIPT_PATH}/../lib/jnomics.jar edu.cshl.schatz.jnomics.manager.server.JnomicsDataServer > /tmp/jnomics-data-server.log  &
