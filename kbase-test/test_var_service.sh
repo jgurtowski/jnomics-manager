1395d4ecaeeb3ccc6d554a5fef58f05efaf12181
james <gurtowsk@cshl.edu>
2012-10-04 13:07:12 -0400

    updated dependency and automated testing

--- kbase-test/test_var_service.sh
+++ kbase-test/test_var_service.sh
@@ -24,20 +24,20 @@ function jmessage {
 
 #remove local files
 jmessage "Removing stale files"
-rm -f yeastrename.1.fq yeastrename.2.fq 
-rm -f yeastrename_test.vcf
+rm -f yeast_sim.1.fq.gz yeast_sim.2.fq.gz 
+rm -f yeast_sim.vcf
 
 #run new stuff
 jmessage "Downloading test data"
-jkbase fs -get /share/yeastrename.1.fq
-jkbase fs -get /share/yeastrename.2.fq
+jkbase fs -get /share/example/yeast_sim.1.fq.gz
+jkbase fs -get /share/example/yeast_sim.2.fq.gz
 
 jmessage "Uploading test data to cluster"
-jkbase fs -put_pe yeastrename.1.fq yeastrename.2.fq ${TEST_DIR}/yeastrename_test
+jkbase fs -put_pe yeast_sim.1.fq.gz yeast_sim.2.fq.gz ${TEST_DIR}/yeast_sim
 
 jmessage "Aligning reads"
 #run alignment
-align_jobid=`jkbase compute bwa -in ${TEST_DIR}/yeastrename_test.pe -out ${TEST_DIR}/yeastrename_test_bwa -organism yeastrename | cut -d ':' -f 2`
+align_jobid=`jkbase compute bwa -in ${TEST_DIR}/yeast_sim.pe -out ${TEST_DIR}/yeast_sim_bwa -organism yeast | cut -d ':' -f 2`
 echo "alignment job: ${align_jobid}"
 
 while [ `jkbase compute status -job $align_jobid | grep Complete | awk '{print $2}'` == "false" ]
@@ -48,7 +48,7 @@ done
 
 jmessage "Calling snps"
 #run snp
-snp_jobid=`jkbase compute snp -in ${TEST_DIR}/yeastrename_test_bwa -out ${TEST_DIR}/yeastrename_test_snp -organism yeastrename | cut -d ':' -f 2`
+snp_jobid=`jkbase compute snp -in ${TEST_DIR}/yeast_sim_bwa -out ${TEST_DIR}/yeast_sim_snp -organism yeast | cut -d ':' -f 2`
 echo "snp job: ${snp_jobid}"
 
 while [ `jkbase compute status -job $snp_jobid | grep Complete | awk '{print $2}'` == "false" ]
@@ -59,13 +59,13 @@ done
 
 jmessage "Merging vcf files"
 #merge vcf
-jkbase compute vcf_merge -alignments ${TEST_DIR}/yeastrename_test_bwa -in ${TEST_DIR}/yeastrename_test_snp -out ${TEST_DIR}/yeastrename_test.vcf
+jkbase compute vcf_merge -alignments ${TEST_DIR}/yeast_sim_bwa -in ${TEST_DIR}/yeast_sim_snp -out ${TEST_DIR}/yeast_sim.vcf
 
 jmessage "Downloading complete vcf"
 #download vcf
-jkbase fs -get ${TEST_DIR}/yeastrename_test.vcf
+jkbase fs -get ${TEST_DIR}/yeast_sim.vcf
 
 jmessage "Tests Complete: PASSED, Have a nice day :)"
 
-rm -f yeastrename.1.fq yeastrename.2.fq 
+rm -f yeast_sim.1.fq.gz yeast_sim.2.fq.gz 
 
