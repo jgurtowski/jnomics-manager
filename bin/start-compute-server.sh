dc42c5b00186b13198bdf7ab41932faea4fd2652
james <gurtowsk@cshl.edu>
2013-10-21 14:36:12 -0400

    restored files

---
+++ bin/start-compute-server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SCRIPT_PATH=`dirname "$0"`
+
+classp=`find ${SCRIPT_PATH}/../lib/*.jar ${SCRIPT_PATH}/../dist/*.jar 2> /dev/null | awk  '{ s=s$1":"} END{print s}'`
+nohup java -Djkserver_keystore=${SCRIPT_PATH}/../cert/keystore.jks -cp ${SCRIPT_PATH}/../conf:${classp} edu.cshl.schatz.jnomics.manager.server.JnomicsComputeServer > /tmp/jnomics-compute-server.log &
